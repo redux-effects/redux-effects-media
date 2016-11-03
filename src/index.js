50c9324252c3d1fa6e6d9119f4bdf4b34728c16c
Andrew Shaffer <andrew@weo.io>
2016-11-03 14:34:40 -0700

    fix firefox support for print media query; make it a noop if its required in non-browser environment

--- src/index.js
+++ src/index.js
@@ -15,6 +15,13 @@ const WATCH_MEDIA = '@@REDUX_EFFECTS/WATCH_MEDIA'
  */
 
 function middleware ({dispatch}) {
+  // This middleware is a noop for non-browser environments
+  if (typeof window === 'undefined') {
+    return next => action => next(action)
+  }
+
+  const isFirefox = navigator.userAgent.toLowerCase().indexOf('firefox') > -1
+
   // Don't load the polyfill unless the middleware is actually going to be used. We don't want just requiring the
   // package (e.g. to grab the action creator) to break on the server.
   require('matchmedia-polyfill')
@@ -22,25 +29,24 @@ function middleware ({dispatch}) {
 
   return next => action => {
     if (action.type === WATCH_MEDIA) {
-      return doWatch(action.payload)
-    }
+      const {queries, cb} = action.payload
 
-    return next(action)
-  }
-
-  /**
-   * doWatch
-   */
-
-  function doWatch ({queries, cb}) {
-    forEach(
-      (mq, key) => {
+      forEach((mq, key) => {
         const mql = matchMedia(mq)
         mql.addListener(mql => dispatch(cb({key, matches: mql.matches})))
+
+        if (mq === 'print' && isFirefox) {
+          // Correct for the fact that media query listeners do not work with printing
+          // in firefox, but these before/after print queries do
+          window.addEventListener('beforeprint', () => dispatch(cb({key, matches: true})))
+          window.addEventListener('afterprint', () => dispatch(cb({key, matches: false})))
+        }
+
         dispatch(cb({key, matches: mql.matches}))
-      },
-      queries
-    )
+      }, queries)
+    } else {
+      return next(action)
+    }
   }
 }
 
